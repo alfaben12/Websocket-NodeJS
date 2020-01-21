b3fed64f5541fd237227f11749c4b72ad2539c51
Thariqalfa <zrav420@gmail.com>
2020-01-21 12:29:09 +0700

    update port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = app.get('port');
